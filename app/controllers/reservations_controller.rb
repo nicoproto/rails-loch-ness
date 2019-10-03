c2cc0db0c176a1c9925e0dfd8c124bb0ddf8d321
Sy Rashid <sy.rashid.11@gmail.com>
2019-10-02 21:50:32 -0700

    STRUCT: Implemented reservations controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,12 @@
+class ReservationsController < ApplicationController
+  new create show edit update
+  def new; end
+
+  def create; end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+end
