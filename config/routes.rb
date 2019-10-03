6fccb935c54e0a3c57811c641300f3db61dc339c
Sy Rashid <sy.rashid.11@gmail.com>
2019-10-02 21:48:12 -0700

    STRUCT: Created routes for reviews and for messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :monsters
+  resources :monsters do
+    resources :reviews, only: %i[new create show index]
+  end
+  resources :reservations, only: %i[new create show edit update]
+  resources :messages, only: %i[new create]
 end
