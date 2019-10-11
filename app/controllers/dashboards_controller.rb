e378af7f691ab50969909d20974c095216debae0
Nico Proto <nicolasproto@gmail.com>
2019-10-11 19:32:00 +0200

    FEAT: Added dashboard functionallity splitted by status

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,8 @@
 class DashboardsController < ApplicationController
   def show
+    @reservations = Reservation.all.select { |reservation| reservation.monster.user.id == current_user.id }
+    @pending_reservations = Reservation.all.select { |reservation| reservation.monster.user.id == current_user.id && reservation.status == "pending" }
+    @accepted_reservations = Reservation.all.select { |reservation| reservation.monster.user.id == current_user.id && reservation.status == "confirmed" }
+    @rejected_reservations = Reservation.all.select { |reservation| reservation.monster.user.id == current_user.id && reservation.status == "denied" }
   end
 end
