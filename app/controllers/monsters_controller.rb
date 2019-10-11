2ca1737b2773ecc41127473f45c07fa301bfc05a
Sy Rashid <sy.rashid.11@gmail.com>
2019-10-12 00:24:40 +1100

    FEAT: Implemented search by available monsters

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,18 +1,26 @@
+require 'pry'
 class MonstersController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[index show price_filter_asc price_filter_dsc review_filter_asc review_filter_dsc]
 
   def index
-    if params[:serach_date].present?
-      dates = params[:search_date].split(" to ")
+    @monsters = Monster.geocoded
+
+    @monsters = @monsters.near(params[:location], 100) if params[:location].present?
+
+   if params[:search_date].present?
+      dates = params[:search_date].split(' to ')
       start_date = Date.parse(dates[0])
       end_date = Date.parse(dates[1])
-    end
 
-    if params[:location].present?
-      @monsters = Monster.near(params[:location], 100)
-    else
-      @monsters = Monster.geocoded
-    end
+      reservations = @monsters.collect(&:reservations).flatten
+      conflict_reservations = reservations.select do |reservation|
+        reservation.overlaps?(start_date, end_date) && reservation.status == 'confirmed'
+      end
+
+      conflict_monsters = conflict_reservations.collect(&:monster).uniq
+
+      @monsters = @monsters.reject { |monster| conflict_monsters.include?(monster) }
+  end
 
     @markers = @monsters.map do |monster|
       {
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,6 +1,7 @@
 class Monster < ApplicationRecord
   belongs_to :user
   has_many :reviews, dependent: :destroy
+  has_many :reservations, dependent: :destroy
 
   validates :name, presence: true
   validates :photo, presence: true
