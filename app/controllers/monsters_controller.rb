4e241c61e71aaa55dfd15c9a07edcc2df0110372
Sy Rashid <sy.rashid.11@gmail.com>
2019-10-12 00:34:55 +1100

    FEAT: refactored search by available monster code

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -4,24 +4,8 @@ class MonstersController < ApplicationController
 
   def index
     @monsters = Monster.geocoded
-
     @monsters = @monsters.near(params[:location], 100) if params[:location].present?
-
-   if params[:search_date].present?
-      dates = params[:search_date].split(' to ')
-      start_date = Date.parse(dates[0])
-      end_date = Date.parse(dates[1])
-
-      reservations = @monsters.collect(&:reservations).flatten
-      conflict_reservations = reservations.select do |reservation|
-        reservation.overlaps?(start_date, end_date) && reservation.status == 'confirmed'
-      end
-
-      conflict_monsters = conflict_reservations.collect(&:monster).uniq
-
-      @monsters = @monsters.reject { |monster| conflict_monsters.include?(monster) }
-  end
-
+    @monsters = filter_by_date(params[:search_date]) if params[:search_date].present?
     @markers = @monsters.map do |monster|
       {
         lat: monster.latitude,
@@ -50,20 +34,42 @@ class MonstersController < ApplicationController
   def price_filter_asc
     @monsters = Monster.order(price: :asc)
     @monsters = @monsters.near(params[:location], 100) if params[:location].present?
+    @monsters = filter_by_date(params[:search_date]) if params[:search_date].present?
+    binding.pry
   end
 
   def price_filter_dsc
     @monsters = Monster.order(price: :desc)
     @monsters = @monsters.near(params[:location], 100) if params[:location].present?
+    @monsters = filter_by_date(params[:search_date]) if params[:search_date].present?
   end
 
   def review_filter_asc
     @monsters = Monster.order(avg_reviews: :asc)
     @monsters = @monsters.near(params[:location], 100) if params[:location].present?
+    @monsters = filter_by_date(params[:search_date]) if params[:search_date].present?
   end
 
   def review_filter_dsc
     @monsters = Monster.order(avg_reviews: :desc)
     @monsters = @monsters.near(params[:location], 100) if params[:location].present?
+    @monsters = filter_by_date(params[:search_date]) if params[:search_date].present?
+  end
+
+  private
+
+  def filter_by_date(date_string)
+    dates = date_string.split(' to ')
+    start_date = Date.parse(dates[0])
+    end_date = Date.parse(dates[1])
+
+    reservations = @monsters.collect(&:reservations).flatten
+    conflict_reservations = reservations.select do |reservation|
+      reservation.overlaps?(start_date, end_date) && reservation.status == 'confirmed'
+    end
+
+    conflict_monsters = conflict_reservations.collect(&:monster).uniq
+
+    @monsters.reject { |monster| conflict_monsters.include?(monster) }
   end
 end
