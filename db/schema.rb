a5726b87d025d2fb2ea2ba93af0960f256fb6e34
Nico Proto <nicolasproto@gmail.com>
2019-10-15 09:41:08 +0200

    FIX: Removed photo from model and added later with Cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_14_041056) do
+ActiveRecord::Schema.define(version: 2019_10_14_152212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2019_10_14_041056) do
   create_table "monsters", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.string "photo"
     t.decimal "price"
     t.float "latitude"
     t.float "longitude"
@@ -46,6 +45,7 @@ ActiveRecord::Schema.define(version: 2019_10_14_041056) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.float "avg_reviews", default: 0.0
+    t.string "photo"
     t.index ["user_id"], name: "index_monsters_on_user_id"
   end
 
