3bf68e58f105b1f4689ba14d4bb737ab936b2173
Nico Proto <nicolasproto@gmail.com>
2019-10-15 09:38:49 +0200

    FIX: Updated seeds to connect to cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,7 +43,7 @@ monster1 = Monster.create(
   description: 'The Chimera is a ferocious, fire breathing monstrosity that possessed the body and head of a lion with the head of a goat protruding from it’s back and a snake for a tail... Skills: Ice water, Laser arch, fire ball, ',
   name: 'Meduphon',
   user: nico,
-  photo: 'https://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2016/05/creature_from_the_black_lagoon_franchise_retrospective.jpg?itok=oow2GS2l'
+  remote_photo_url: 'https://cdn2us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2016/05/creature_from_the_black_lagoon_franchise_retrospective.jpg?itok=oow2GS2l'
 )
 monster2 = Monster.create(
   address: 'Lisbon',
@@ -51,7 +51,7 @@ monster2 = Monster.create(
   description: 'The monster is feared and believed to have been an omen for storms, shipwrecks and other natural disasters... Skills: Laser eyes, Super punch, Immortality',
   name: 'Typhon',
   user: nico,
-  photo: 'https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1922&q=80'
+  remote_photo_url: 'https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1922&q=80'
 )
 monster3 = Monster.create(
   address: 'Moscow',
@@ -59,7 +59,7 @@ monster3 = Monster.create(
   description: 'The monster is perhaps little known and does not appear in any traditional epic or popular legend. However her frightening appearance, and her ghastly tendency to feast on human blood and flesh, more than warrants her place as number seven on our list...  Skills: Fire breath, killer jump, Flying',
   name: 'Medusa',
   user: nico,
-  photo: 'https://vignette.wikia.nocookie.net/movie-monster/images/e/e3/GiantBehemoth.png/revision/latest?cb=20180606174201'
+  remote_photo_url: 'https://vignette.wikia.nocookie.net/movie-monster/images/e/e3/GiantBehemoth.png/revision/latest?cb=20180606174201'
 )
 monster4 = Monster.create(
   address: 'Warsaw',
@@ -67,63 +67,63 @@ monster4 = Monster.create(
   description: 'This monster has the confounding ability to regrow any decapitated limbs with alarming speed... Skills: Assault Ribbon, Laser blade',
   name: 'The Minotaur',
   user: nico,
-  photo: 'https://www.irishexaminer.com/remote/media.central.ie/media/images/a/AMonsterCallsScreenshot_large.jpg')
+  remote_photo_url: 'https://www.irishexaminer.com/remote/media.central.ie/media/images/a/AMonsterCallsScreenshot_large.jpg')
 monster5 = Monster.create(
   address: 'Berlin',
   price: 110,
   description: 'Cerberus originally attacks with either a sickle, a sword, or his trademark club... Skills: Leaping Spider, flicker strike',
   name: 'Cerberus',
   user: nico,
-  photo: 'https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1922&q=80')
+  remote_photo_url: 'https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1922&q=80')
 monster6 = Monster.create(
   address: 'Oslo',
   price: 160,
   description: 'It is said that the beast only had an appetite for living flesh and so would only allow the deceased spirits to pass, while consuming any living mortal who is foolish enough to come near him... Skills: Raise Zombie, Bone Cruncher, Immortality',
   name: 'Charybdis',
   user: nico,
-  photo: 'https://media.npr.org/assets/img/2012/10/10/1750_bw_00011ad_wide-c577aaf6b246d5688d05ff05c81e2c157c568290-s800-c85.jpg')
+  remote_photo_url: 'https://media.npr.org/assets/img/2012/10/10/1750_bw_00011ad_wide-c577aaf6b246d5688d05ff05c81e2c157c568290-s800-c85.jpg')
 monster7 = Monster.create(
   address: 'Stockholm',
   price: 88,
   description: 'This monster is best remembered for his affinity for devouring flesh and his cryptic home, deep within the confines of the twisted labyrinth... Skills: Fireball, Lightning Warp, Immortality',
   name: 'Hydra',
   user: sy,
-  photo: 'http://cdn.collider.com/wp-content/uploads/2017/06/godzilla-1954.jpg')
+  remote_photo_url: 'http://cdn.collider.com/wp-content/uploads/2017/06/godzilla-1954.jpg')
 monster8 = Monster.create(
   address: 'Lyon',
   price: 160,
   description: 'The supernatural forces that sustain this vampire beyond mortal death also endow them with immortality, heightened senses, and enhanced superhuman physical abilities as well as powers of mind control and mental abilities which make them physically superior to humans... Skills: Arctic breath, Cold Snap',
   name: 'Empusa',
   user: sy,
-  photo: 'https://i1.wp.com/faroutmagazine.co.uk/wp-content/uploads/2019/07/Godzilla-1954.jpeg?w=1200&ssl=1')
+  remote_photo_url: 'https://i1.wp.com/faroutmagazine.co.uk/wp-content/uploads/2019/07/Godzilla-1954.jpeg?w=1200&ssl=1')
 monster9 = Monster.create(
   address: 'Rome',
   price: 125,
   description: 'This monster has the ability to adapt to sunlight and the power to render themselves invisible provided they are powerful enough... Skills: Burning Arrow, Cold Snap Immortality' ,
   name: 'Chimera',
   user: sy,
-  photo: 'https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2019/02/dream.jpg?ssl=1')
+  remote_photo_url: 'https://i0.wp.com/bloody-disgusting.com/wp-content/uploads/2019/02/dream.jpg?ssl=1')
 monster10 = Monster.create(
   address: 'Paris',
   price: 170,
   description: 'This is a monster that is dangerous because it can mimic a humans voice. It can use the voices of loved ones to lure victims into traps that it has set up... Skills: Lightning Arrow, Immortality',
   name: 'Cyclops',
   user: sy,
-  photo: 'http://getwallpapers.com/wallpaper/full/b/6/a/500920.jpg')
+  remote_photo_url: 'http://getwallpapers.com/wallpaper/full/b/6/a/500920.jpg')
 monster11 = Monster.create(
   address: 'Valencia',
   price: 190,
   description: 'This monster her ghastly tendency to feast on human blood and flesh, more than warrants her place as number seven on our list... Skills: Explosive Arrow, Ice Shot',
   name: 'Sphinx',
   user: sy,
-  photo: 'https://gallery.mailchimp.com/e0f5618a41f76b17c22005aec/images/cy.gif')
+  remote_photo_url: 'https://gallery.mailchimp.com/e0f5618a41f76b17c22005aec/images/cy.gif')
 monster12 = Monster.create(
   address: 'Istanbul',
   price: 188,
   description: 'This monster is so feaurious and has the ability to grow stronger with time, age, and the consumption of human blood... Skills: Immortality, golden horn, super punch',
   name: 'Cycsphira',
   user: sy,
-  photo: 'https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1922&q=80')
+  remote_photo_url: 'https://images.unsplash.com/photo-1509248961158-e54f6934749c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1922&q=80')
 
 printLine
 
