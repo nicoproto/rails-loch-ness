74b9d49cab530aa94ce538aa2b53daf3b8712ff2
Sy Rashid <sy.rashid.11@gmail.com>
2019-10-15 12:22:39 +1100

    FEAT: Implemented owner conversations from seller side

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,6 +1,7 @@
 class ConversationsController < ApplicationController
   def index
     @conversations = Conversation.where(user: current_user)
+    @owner_conversations = Conversation.all.select { |convo| convo.monster.user == current_user }
   end
 
   def new; end
