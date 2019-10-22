607a25b8c7c794328df4c4d2f8cec5cd31c3481c
Sy Rashid <sy.rashid.11@gmail.com>
2019-10-22 11:01:42 +1100

    STRUCT: Added file structure for mailers

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,35 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.pending.subject
+  #
+  def pending
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.request.subject
+  #
+  def request
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.confirmation.subject
+  #
+  def confirmation
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
