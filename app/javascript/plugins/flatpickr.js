99a68d6f93b7067940f0fdc7972708e6b6e97bb2
Sy Rashid <sy.rashid.11@gmail.com>
2019-10-23 13:54:05 +1100

    FEAT: No longer able to pick monster dates already picked

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,10 +1,12 @@
 require("flatpickr/dist/themes/material_green.css");
 
 const milisecs_to_days = (24*60*60*1000);
+const unvailableDates = JSON.parse(document.querySelector('.widget-content').dataset.unavailable);
 
 flatpickr("#datepicker", {
   mode: 'range',
   minDate: "today",
+  disable: unvailableDates,
   dateFormat: "Y-m-d",
   onClose: function(selectedDates, dateStr, instance) {
       const booking_time = (selectedDates[1] - selectedDates[0]) / milisecs_to_days + 1;
@@ -18,5 +20,6 @@ flatpickr("#datepicker", {
 flatpickr("#datesearch", {
   mode: 'range',
   minDate: 'today',
+  disable: ["2019-11-26"],
   dateFormat: "Y-m-d"
 });
